83551b3630bf417643556d592660cc9e6dd6ceed
小涛 <1073519986@qq.com>
2021-03-10 12:08:00 +0800

    添加依赖

--- plugin/ac_cron/services/UserParentService.php
+++ plugin/ac_cron/services/UserParentService.php
@@ -12,34 +12,40 @@
 // +----------------------------------------------------------------------
 // | Author：Orzice(小涛)  https://gitee.com/orzice
 // +----------------------------------------------------------------------
-// | DateTime：2021-01-04 17:21:13
+// | DateTime：2021-02-21 14:16:19
 // +----------------------------------------------------------------------
-namespace HaSog\plugin\a\listener;
+namespace HaSog\plugin\ac_cron\services;
 
+use app\common\model\Member;
+use app\common\service\ParentService;
 
-use think\Event;
 /**
-* 
-*/
-class Index 
+ * 用户相关计划任务
+ */
+class UserParentService
 {
-	public function subscribe(Event $event){
-		//$event->listen('UserLogin', [$this,'onUserLogin']);
-                $event->listen('UserLogin', [$this,'onUserLogin']);
-                print_r("事件监听注册<br>");
-             
-        // $event->listen(\app\api\controller\index::class, function ($event) {
-        //     //订单model
-        //     print_r($event);
-        //     exit;
-        //     // $model = $event->getOrderModel();
-        //     // $this->handle($model);
-        // });
+    public function handle()
+    {
+        $this->model = new Member();
+        $time = time();
+        $sql = $this->model->where('parent_ids_s',0)->limit(40)->select();
+        if(count($sql) == 0){
+            return;
         }
-        public function onUserLogin($user)
-        {
-            // UserLogin事件响应处理
-            print_r("插件A 响应<br>数据:".$user."<br>");
+        
+        foreach ($sql as $u) {
+            $this->user($u);
         }
 
+    }
+    function user($user){
+        $this->model = new Member();
+        $data = new ParentService();
+        if ($user['parent_ids_s'] !== 0) {
+            return;
+        }
+        $parent_ids = $data->ParentInit($user['parent_id']);
+        $this->model->where('id',$user['id'])->update(['parent_ids_s'=>1,'parent_ids'=>$parent_ids]);
+        return;
+    }
 }
