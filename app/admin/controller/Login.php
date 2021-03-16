9bc97b36255c34195d7a0b6da0fd28aefe1fb98a
小涛 <1073519986@qq.com>
2021-03-16 11:43:25 +0800

    更新部分代码

--- app/admin/controller/Login.php
+++ app/admin/controller/Login.php
@@ -62,10 +62,10 @@ class Login extends AdminController
             $this->validate($post, $rule);
             $admin = SystemAdmin::where(['username' => $post['username']])->find();
             if (empty($admin)) {
-                $this->error('用户不存在');
+                $this->error('账号不存在或密码输入有误');
             }
             if (password($post['password']) != $admin->password) {
-                $this->error('密码输入有误');
+                $this->error('账号不存在或密码输入有误');
             }
             if ($admin->status == 0) {
                 $this->error('账号已被禁用');
