8d26b13d4a57a6129d74440ace8401cbc3c5b104
小涛 <1073519986@qq.com>
2021-03-22 10:00:52 +0800

    演示站配置准备

--- app/admin/controller/admins/Payment.php
+++ app/admin/controller/admins/Payment.php
@@ -1,274 +1,142 @@
 <?php
-
 // +----------------------------------------------------------------------
-
 // | HaSog (幻神商城系统)
-
 // +----------------------------------------------------------------------
-
 // | 技术支持【幻神科技】: https://www.hasog.com
-
 // +----------------------------------------------------------------------
-
 // | 联系我们:  https://www.hasog.com
-
 // +----------------------------------------------------------------------
-
 // | gitee开源项目：https://gitee.com/orzice/hasog
-
 // +----------------------------------------------------------------------
-
 // | github开源项目：https://github.com/orzice/hasog
-
 // +----------------------------------------------------------------------
-
 // | Author：梗集(王国骁)  https://gitee.com/orzice
-
 // +----------------------------------------------------------------------
-
 // | DateTime：2020-12-31 18:12:04
-
 // +----------------------------------------------------------------------
-
-
 
 namespace app\admin\controller\admins;
 
-
-
 use app\common\controller\AdminController;
-
 use app\common\model\AdminsPayment as Payments;
-
 use think\App;
 
-
-
 use EasyAdmin\annotation\ControllerAnnotation;
-
 use EasyAdmin\annotation\NodeAnotation;
 
-
-
 /**
-
  * Class Test
-
  * @package app\admin\controller\page
-
  * @ControllerAnnotation(title="线下付款配置")
-
  */
-
 class Payment extends AdminController
-
 {
-
     use \app\admin\traits\Curd;
 
-
-
     protected $sort = [
-
         'create_time' => 'desc',
-
     ];
 
-
-
     public function __construct(App $app)
-
     {
-
         parent::__construct($app);
-
         $this->model = new Payments();
-
     }
-
     /**
-
      * @NodeAnotation(title="线下付款配置列表")
-
      */
-
     public function index()
-
     {
-
         if ($this->request->isAjax()) {
-
             if (input('selectFields')) {
-
                 return $this->selectList();
-
             }
-
             list($page, $limit, $where) = $this->buildTableParames();
-
             $count = $this->model
-
                 ->where($where)
-
                 ->count();
-
             $list = $this->model
-
                 ->where($where)
-
                 ->page($page, $limit)
-
                 ->order($this->sort)
-
                 ->select();
-
             $data = [
-
                 'code' => 0,
-
                 'msg' => '',
-
                 'count' => $count,
-
                 'data' => $list,
-
             ];
-
             return json($data);
-
         }
-
         return $this->fetch();
-
     }
-
     /**
-
      * @NodeAnotation(title="线下付款配置添加")
-
      */
-
     public function add()
-
     {
-
         if ($this->request->isAjax()) {
-
+            
             if(env('hasog.is_demo', false)){
                 $this->error('演示环境下不允许修改');
             }
             $post = $this->request->post();
-
             $rule = [
-
                 'accnumber|开户卡号' => 'require|number',
-
                 'thumb|二维码' => 'require|url',
-
                 'accname|开户名' => 'require',
-
                 'name|名称' => 'require',
-
                 'bankdeposit|开户行' => 'require',
-
                 'bankdeposits|开户支行' => 'require',
-
                 'state|状态' => 'require|number|in:0,1',
-
             ];
-
             $this->validate($post, $rule);
-
             $post['qrcode'] = $post['thumb'];
-
             Uploadfile($post['qrcode']);
-
             unset($post['thumb']);
-
             unset($post['file']);
-
             try {
-
                 $save = $this->model->save($post);
-
             } catch (\Exception $e) {
-
                 $this->error('保存失败:'.$e->getMessage());
-
             }
-
             $save ? $this->success('保存成功') : $this->error('保存失败');
-
         }
-
         return $this->fetch();
-
     }
-
     /**
-
      * @NodeAnotation(title="线下付款配置修改")
-
      */
-
     public function edit($id)
-
     {
-
         $row = $this->model->find($id);
-
         $row->isEmpty() && $this->error('数据不存在');
-
         if ($this->request->isAjax()){
 
             if(env('hasog.is_demo', false)){
                 $this->error('演示环境下不允许修改');
             }
-            
             $post = $this->request->post();
-
             $rule = [
-
                 'accnumber|权重' => 'require|number',
-
                 'thumb|二维码' => 'require|url',
-
                 'accname|开户名' => 'require',
-
                 'bankdeposit|开户行' => 'require',
-
                 'bankdeposits|开户支行' => 'require',
-
                 'state|状态' => 'require|number|in:0,1',
-
             ];
-
             $this->validate($post, $rule);
-
             $post['qrcode'] = $post['thumb'];
-
             unset($post['thumb']);
-
             unset($post['file']);
-
             try {
-
                 $save = $this->model->where('id',$post['id'])->update($post);
-
             } catch (\Exception $e) {
-
                 $this->error('保存失败:'.$e->getMessage());
-
             }
-
             $save ? $this->success('保存成功') : $this->error('保存失败');
-
         }
-
         $this->assign('row',$row);
-
         return $this->fetch();
-
     }
-
 }
--- app/admin/controller/finace/Withdrawset.php
+++ app/admin/controller/finace/Withdrawset.php
@@ -1,224 +1,115 @@
 <?php
-
 // +----------------------------------------------------------------------
-
 // | HaSog (幻神商城系统)
-
 // +----------------------------------------------------------------------
-
 // | 技术支持【幻神科技】: https://www.hasog.com
-
 // +----------------------------------------------------------------------
-
 // | 联系我们:  https://www.hasog.com
-
 // +----------------------------------------------------------------------
-
 // | gitee开源项目：https://gitee.com/orzice/hasog
-
 // +----------------------------------------------------------------------
-
 // | github开源项目：https://github.com/orzice/hasog
-
 // +----------------------------------------------------------------------
-
 // | Author：梗集(王国骁)  https://gitee.com/orzice
-
 // +----------------------------------------------------------------------
-
 // | DateTime：2020-12-31 18:12:48
-
 // +----------------------------------------------------------------------
-
-
 
 namespace app\admin\controller\finace;
-
 use app\common\controller\AdminController;
-
 use app\common\model\FinaceWithdrawset as Withdrawsets;
-
 use think\App;
 
-
-
 use EasyAdmin\annotation\ControllerAnnotation;
-
 use EasyAdmin\annotation\NodeAnotation;
-
 /**
-
  * Class Test
-
  * @package app\admin\controller\page
-
  * @ControllerAnnotation(title="提现设置")
-
  */
-
 class Withdrawset extends AdminController
-
 {
-
     use \app\admin\traits\Curd;
 
-
-
     protected $sort = [
-
         'create_time'   => 'desc',
-
     ];
 
-
-
     public function __construct(App $app)
-
     {
-
         parent::__construct($app);
-
         $this->model = new Withdrawsets();
-
     }
-
     /**
-
      * @NodeAnotation(title="提现设置修改")
-
      */
-
     public function edit(){
-
         $res = $this->model->limit(1)->select()->toArray();
-
         if (empty($res)){
-
             $res = array([
-
                 'recharge'=>0,
-
                 'wechat'=>0,
-
                 'wechatmin'=>'',
-
                 'wechatnum'=>'',
-
                 'wechatmax'=>'',
-
                 'alipay'=>0,
-
                 'alipaymin'=>'',
-
                 'alipaymax'=>'',
-
                 'alipaynum'=>'',
-
                 'manual'=>0,
-
                 'manual_wechat'=>0,
-
                 'manual_alipay'=>0,
-
                 'service'=>0,
-
                 'services'=>0,
-
                 'servicesminus'=>0,
-
                 'minservices'=>0,
-
             ]);
-
         }
-
         $row='';
-
         foreach($res as $k=>$v){
-
             $row = $v;
-
         }
-
         if ($this->request->isAjax()){
-
+            
             if(env('hasog.is_demo', false)){
                 $this->error('演示环境下不允许修改');
             }
             $post = $this->request->post();
 
-
-
             $rule = [
-
                 'recharge'=>'require|number|in:0,1',
-
                 'wechat'=>'require|number|in:0,1',
-
                 'wechatmin|微信最低金额'=>'float|between: 0.3,20000',
-
                 'wechatmax|微信最高金额'=>'float|between: 0.3,20000',
-
                 'wechatnum|微信提现次数'=>'number|between: 1,10',
-
                 'alipay'=>'require|number|in:0,1',
-
                 'alipaymin|支付宝最低金额'=>'float|between: 0,50000',
-
                 'alipaymax|支付宝最高金额'=>'float|between: 0,50000',
-
                 'alipaynum|支付宝提现次数'=>'number|min:1',
-
                 'manual'=>'require|number|in:0,1',
-
                 'manual_wechat'=>'require|number|in:0,1',
-
                 'manual_alipay'=>'require|number|in:0,1',
-
                 'service'=>'require|number|in:0,1',
-
                 'services'=>'float|between: 0,999999',
-
                 'servicesminus'=>'float|between: 0,999999',
-
                 'minservices'=>'float|between: 0,999999',
-
             ];
-
             $this->validate($post['balance'], $rule);
-
             $res = $this->model->limit(1)->select()->toArray();
-
             try {
-
                 if (empty($res)){
 
-
-
                     $save = $this->model->insert($post['balance']);
-
                 }else{
-
                     $post['balance']['id'] = $res['0']['id'];
-
                     $save = $this->model->update($post['balance']);
-
                 }
-
             } catch (\Exception $e) {
-
                 $this->error('保存失败:'.$e->getMessage());
-
             }
-
             $save ? $this->success('保存成功') : $this->error('保存失败');
-
         }
-
         $this->assign('res',$row);
-
        return $this->fetch();
-
     }
-
 }
